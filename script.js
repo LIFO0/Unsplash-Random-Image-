448437702ffbeae603dc559b60f88f9dd7c1336b
LIFO0 <167644668+LIFO0@users.noreply.github.com>
2025-04-24 18:36:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ async function changeImageFunc() {
 
         await cacheImage(imageUrl);
         localStorage.setItem("lastImageUrl", imageUrl);
+        localStorage.setItem("lastCreatorName", jsonData.user.name);
+        localStorage.setItem("lastImageLink", jsonData.links.html);
         localStorage.setItem("lastLoadTime", new Date().getTime().toString());
         // imageElement.src = jsonData.urls.regular;
         document.body.style.backgroundImage = `url(${imageUrl})`;
@@ -48,6 +50,8 @@ async function getCachedImage(url) {
 
 async function loadImageOnStart() {
     const lastImageUrl = localStorage.getItem("lastImageUrl");
+    const lastCreatorName = localStorage.getItem("lastCreatorName");
+    const lastImageLink = localStorage.getItem("lastImageLink");
     const lastLoadTime = localStorage.getItem("lastLoadTime");
     const now = new Date().getTime();
     const twelveHouse = 12 * 60 * 60 * 1000;
@@ -55,6 +59,9 @@ async function loadImageOnStart() {
         const cachedImage = await getCachedImage(lastImageUrl);
         if (cachedImage) {
             document.body.style.backgroundImage = `url(${cachedImage})`;
+            creatorElement.innerText = lastCreatorName;
+            creatorElement.setAttribute("href", lastImageLink);
+
             return;
         }
     }
